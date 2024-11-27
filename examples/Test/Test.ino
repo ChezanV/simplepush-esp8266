31bac9f8c28d0b9da34ac8b38e7b5e8f5524a3cf
ChezanV <chezan_v@yahoo.com>
2024-11-27 14:22:09 +0200

    Update Test.ino

--- examples/Test/Test.ino
+++ examples/Test/Test.ino
@@ -1,1 +1,37 @@
+#include <Arduino.h>
 
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <Simplepush.h>
+
+
+
+ESP8266WiFiMulti WiFiMulti;
+Simplepush simple;
+
+void setup() {
+	Serial.begin(115200);
+	Serial.setDebugOutput(true);
+
+	Serial.println();
+	Serial.println();
+	Serial.println();
+
+	for(uint8_t t = 4; t > 0; t--) {
+		Serial.printf("[SETUP] WAIT %d...\n", t);
+		Serial.flush();
+		delay(1000);
+	}
+
+	WiFiMulti.addAP("YourWifiSSID", "WifiPassword");
+}
+
+void loop() {
+	if((WiFiMulti.run() == WL_CONNECTED)) {
+		simple.send("YourSimplepushKey", "Wow", "This is so easy", "Event");
+		simple.send("YourSimplepushKey", NULL, "No title and no event. Just a message.", NULL);
+		simple.sendEncrypted("YourSimplepushKey", "password", "salt", "Wow", "This is so secure.", "Event");
+		delay(5000);
+	}
+}
